357002150be1e977410cdfbdd8da34c081477520
ZBelka100 <115878049+ZBelka100@users.noreply.github.com>
2023-02-25 14:37:59 +0300

    Update lab26.h

--- lab26.h
+++ lab26.h
@@ -5,4 +5,4 @@ void insert(struct queue *q, int x);
 int isempty(struct queue *q);
 void print(struct queue *q);
 int removes(struct queue *q);
-int qu_size(struct queue *q);
+int quSize(struct queue *q);
